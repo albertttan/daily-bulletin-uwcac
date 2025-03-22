796abb88d93205e0f6472f7046eb0cf730ff4005
Albert Tan <albert-tan@qq.com>
2025-03-22 11:59:32 +0000

    Updated news retrieving section with Google API implementation for collaboration with Peaco News Taskforce

--- scripts/retrieve.py
+++ scripts/retrieve.py
@@ -1,6 +1,16 @@
+import os
+import io
+import html
+import json
 import datetime
 import requests
+import urllib.parse
 from bs4 import BeautifulSoup
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+
 
 
 def retrieve_history(date_iso):
@@ -26,7 +36,33 @@ def retrieve_history(date_iso):
     return [date_history[0], date_history[-1]]
 
 
-def retrieve_news():
+def google_credentials():
+    creds = None
+    SCOPES = ["https://www.googleapis.com/auth/documents.readonly", 
+              "https://www.googleapis.com/auth/drive.readonly", 
+              "https://www.googleapis.com/auth/drive.metadata.readonly"]
+
+    # Retrieve credentials from file
+    if os.path.exists("google-auth/token.json"):
+        with open("google-auth/token.json") as file:
+            token = json.load(file)
+        creds = Credentials.from_authorized_user_info(token)
+    
+    # Log in and save credentials if invalid
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "google-auth/credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("google-auth/token.json", "w") as token:
+            token.write(creds.to_json())
+    
+    return creds
+
+
+def retrieve_news_ap():
 
     # Retrieve page
     url = "https://apnews.com/world-news"
@@ -43,4 +79,34 @@ def retrieve_news():
         del element["class"]
         element.find("span").unwrap()
 
+    output.append(False)
+    return output
+
+
+def retrieve_news(document_id="1ChvbzaBUOMUft4mUmKghzQpI_VnBbOUsBHGxbfyed4w"):
+    creds = google_credentials()
+    service = build("drive", "v3", credentials=creds)
+
+    # Check if file is up-to-date
+    timestamp = service.files().get(fileId=document_id, fields="modifiedTime").execute()["modifiedTime"]
+    from_ap = datetime.datetime.fromisoformat(timestamp).date() != datetime.date.today()
+    if from_ap:
+        return retrieve_news_ap()
+
+    # Retrieve document content as HTML
+    request = service.files().export_media(fileId=document_id, mimeType="text/html")
+    soup = BeautifulSoup(request.execute().decode('utf-8'), "html.parser")
+    
+    # Clean formatting
+    output = []
+    for element in soup.find("ul"):
+        del element["style"]
+        element.find("span").unwrap()
+        link = element.find("a")
+        if link:
+            del link["style"]
+            link["href"] = urllib.parse.parse_qs(urllib.parse.urlparse(link.get("href", "")).query).get("q", [""])[0]
+        output += element.contents
+    
+    output.append(True)
     return output
