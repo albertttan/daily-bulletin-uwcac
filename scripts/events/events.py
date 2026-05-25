72e2941f5b7870acd5a26d01cf36b3ec83b99060
iorinko15 <iori.sawa01@gmail.com>
2026-05-25 21:39:38 +0100

    Daily Bulletin 2026-05-26

--- scripts/events/events.py
+++ scripts/events/events.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Initialization
 
-df = pd.read_excel("events.xlsx", sheet_name="18-24/05", skiprows=2)[
+df = pd.read_excel("events.xlsx", sheet_name="25-/05", skiprows=2)[
     ["Unnamed: 0", "When", "What", "Where"]
 ]
 df.rename(columns={"Unnamed: 0": "Day"}, inplace=True)
